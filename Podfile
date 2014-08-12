f667611f163d931482e26deb55a60fc61404701d
刘廷勇 <liutingyong@Liuty-MacBookPro-2.local>
2014-08-12 17:58:46 +0800

    Add JSONModel

---
+++ Podfile
@@ -0,0 +1,4 @@
+platform:ios, '6.0'
+pod 'AFNetworking', '~> 2.2.4'
+pod 'JSONModel', '~> 0.13.0'
+pod 'AFDownloadRequestOperation', '~> 2.0.1'
---
+++ YDNetwork/IndexModel.m
@@ -0,0 +1,13 @@
+//
+//  IndexModel.m
+//  YDNetwork
+//
+//  Created by 刘廷勇 on 14-8-12.
+//  Copyright (c) 2014年 网易有道. All rights reserved.
+//
+
+#import "IndexModel.h"
+
+@implementation IndexModel
+
+@end
